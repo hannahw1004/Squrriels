37dbd27882da1d4d0e52c1275fd94e9c9f926ab1
jeew0n <79549404+jeew0n@users.noreply.github.com>
2022-10-23 02:37:55 -0700

    finalized layout of ResultHappy page

---
+++ app/src/main/java/com/example/squrriels/ResultMad.java
@@ -0,0 +1,28 @@
+package com.example.squrriels;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ResultMad extends Fragment {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_result_mad, container, false);
+    }
+}
