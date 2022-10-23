a19615d0d62ff79e77dd9ba02f382a4ca42b6208
jeew0n <79549404+jeew0n@users.noreply.github.com>
2022-10-22 23:48:29 -0700

    worked on results page

--- app/src/main/java/com/example/squrriels/ResultHappy.java
+++ app/src/main/java/com/example/squrriels/ResultHappy.java
@@ -8,7 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class Result extends Fragment {
+public class ResultHappy extends Fragment {
 
 
 
